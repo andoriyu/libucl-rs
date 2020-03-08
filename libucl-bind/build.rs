b717ebd110ff88c98b714793ef38e6761a209929
Andrey Cherkashin <with.out@me.com>
2020-03-07 21:29:40 -0800

    Use pkg_config correctly

--- libucl-bind/build.rs
+++ libucl-bind/build.rs
@@ -1,34 +1,19 @@
 extern crate cmake;
+extern crate pkg_config;
 
 use cmake::Config;
 
 
-#[cfg(not(target_os = "freebsd"))]
 fn main() {
-    if std::env::var("LIBUCL_BIND_USE_SYSTEM").is_ok() {
-        use_system_lib_ucl()
-    } else {
-        use_bundled_lib_ucl()
+    let result = pkg_config::Config::new()
+        .atleast_version("0.8.0")
+        .statik(true)
+        .probe("libucl");
+    if result.is_err() {
+        let dst = Config::new("libucl")
+            .no_build_target(true)
+            .build();
+        println!("cargo:rustc-link-search=native={}/build", dst.display());
+        println!("cargo:rustc-link-lib=static=ucl");
     }
-}
-
-#[cfg(target_os = "freebsd")]
-fn main() {
-    if std::env::var("LIBUCL_BIND_USE_BUNDLED").is_ok() {
-        use_bundled_lib_ucl()
-    } else {
-        use_system_lib_ucl()
-    }
-}
-
-fn use_bundled_lib_ucl() {
-    let dst = Config::new("libucl")
-        .no_build_target(true)
-        .build();
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=static=ucl");
-}
-
-fn use_system_lib_ucl() {
-    println!("cargo:rustc-link-lib=ucl");
 }
